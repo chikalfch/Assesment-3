18e31c61c29b959d1592b9c10a78a1b7e352873d
Chikal Fachdiana <chikalfch@student.telkomuniversity.ac.id>
2023-06-15 19:15:58 +0700

    Menampilkan Notifikasi

--- app/src/main/java/org/d3if3056/hitung_luassegitiga/ui/list/ListFragment.kt
+++ app/src/main/java/org/d3if3056/hitung_luassegitiga/ui/list/ListFragment.kt
@@ -1,13 +1,19 @@
 package org.d3if3056.mobpro1.ui.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import org.d3if3056.hitung_luassegitiga.MainActivity
 import org.d3if3056.hitung_luassegitiga.databinding.FragmentListBinding
 import org.d3if3056.hitung_luassegitiga.network.ApiStatus
 import org.d3if3056.hitung_luassegitiga.ui.list.ListBangunDatarAdapter
@@ -50,6 +56,9 @@ class ListFragment : Fragment() {
             }
             ApiStatus.SUCCESS -> {
                 binding.progressBar.visibility = View.GONE
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestNotificationPermission()
+                }
             }
             ApiStatus.FAILED -> {
                 binding.progressBar.visibility = View.GONE
@@ -57,4 +66,20 @@ class ListFragment : Fragment() {
             }
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun requestNotificationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                MainActivity.PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
 }
